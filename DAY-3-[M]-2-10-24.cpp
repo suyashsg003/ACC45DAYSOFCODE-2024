458d06a85898faa7197eeca111f69d6160e982e1
Suyash goyal <135364860+suyashsg003@users.noreply.github.com>
2024-10-02 22:56:25 +0530

    DAY-3-[M]-2-10-24.cpp

---
+++ DAY-3-[M]-2-10-24.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+     int T; 
+    cin >> T;
+    
+    while (T--) {
+        int XA, XB, XC;
+        cin >> XA >> XB >> XC;
+        
+        if (XA > 50) {
+            cout << "A" << endl;
+        } else if (XB > 50) {
+            cout << "B" << endl;
+        } else if (XC > 50) {
+            cout << "C" << endl;
+        } else {
+            cout << "NOTA" << endl;
+        }
+    }
+    return 0;
+}
